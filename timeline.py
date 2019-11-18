390b44b7c8abd05e92035a9b7f3217ef1d927277
Kamikigit <KCkc966@outlook.jp>
2019-11-18 14:55:53 +0000

    post関数追加

--- timeline.py
+++ timeline.py
@@ -8,21 +8,21 @@ twitter = OAuth1Session(keys.CK, keys.CS, keys.AT, keys.ATS)
 app = Flask(__name__)
 
 url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-params ={'count' : 5}
+params ={'count' : 10}
 req = twitter.get(url, params = params)
 
 @app.route('/')
-def main():
+def get_post():  # 1ツイごとにpostsリストに格納
     if req.status_code == 200:
         timeline = json.loads(req.text)
-        result = ''
+        posts = []
         for tweet in timeline:
-            result += (tweet['user']['name']+'\n'+tweet['text'] + '\n')
-            result += (tweet['created_at'] + '\n')
-            result += '------------------------------------' + '\n'
-        return render_template('index.html', result=result)
+            posts.append(tweet['user']['name']+'<br>' \
+                        + tweet['text'] + '<br>' \
+                        + tweet['created_at'] + '<br>')
+        return render_template('index.html', posts=posts)
     else:
-        return "ERROR: %d" % req.status_code)       
+        return "ERROR: %d" % req.status_code       
 
 if __name__=='__main__':
     app.run(host='192.168.33.11',debug=True)
