ff1855ef8e28fa2001d015de034733cc89900a3e
root <root@localhost.localdomain>
2019-12-23 20:53:07 +0900

    media url to image

--- services/app/puchineko/utils.py
+++ services/app/puchineko/utils.py
@@ -1,8 +1,13 @@
-import re
+
 
 def text2link(text, entities):
     for url_info in entities['urls']:
-        marked_up_url = f'<a href=\"{url_info["url"]}\">{url_info["url"]}</a>'
+        marked_up_url = f'<a href=\"{url_info["expanded_url"]}\">{url_info["expanded_url"]}</a>'
         text = text[:url_info["indices"][0]] + marked_up_url + text[url_info["indices"][1]:]
+
+    if 'media' in entities:
+        for media_info in entities['media']:
+            marked_up_url = f'<img src=\"{media_info["media_url"]}\" alt=\"photo:{media_info["display_url"]}\" width=\"250px\" height=\"250px\">'
+            text = text[:media_info['indices'][0]] + text[media_info['indices'][1]:] + "<br>" + marked_up_url
     
     return text
